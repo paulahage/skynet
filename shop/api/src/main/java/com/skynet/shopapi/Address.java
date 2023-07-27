b3e98eb4cad3fa4de82ad953215a229d9d184904
paula hage <hage.paula@hotmail.com>
2023-07-27 14:05:21 +0200

    Implementation of a mock API to make the requests of internet plans and address

---
+++ shop/api/src/main/java/com/skynet/shopapi/Address.java
@@ -0,0 +1,31 @@
+package com.skynet.shopapi;
+
+public class Address implements ApiResponse {
+    private String street;
+    private String city;
+    private String country;
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
---
+++ shop/api/src/main/java/com/skynet/shopapi/ApiController.java
@@ -0,0 +1,67 @@
+package com.skynet.shopapi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController {
+
+    @GetMapping("/getAddress")
+    public ApiResponse getAddressClass(@RequestParam("postcode") String postcode){
+        if(isValidPostcode(postcode)){
+            Address address = new Address();
+            address.setStreet("Zuidplein2");
+            address.setCity("Rotterdam");
+            address.setCountry("Netherlands");
+
+            return address;
+        }else {
+            return null;
+        }
+    }
+
+    @GetMapping("/getPlans")
+    public DataPlans[] getDataPlansClass(@RequestParam("postcode") String postcode){
+
+        if(isValidPostcode(postcode)){
+            DataPlans dataPlans1 = new DataPlans();
+            dataPlans1.setId("01");
+            dataPlans1.setName("Smart");
+            dataPlans1.setDownload("10");
+            dataPlans1.setUpload("2");
+            dataPlans1.setPrice("39.00");
+
+            DataPlans dataPlans2 = new DataPlans();
+            dataPlans2.setId("02");
+            dataPlans2.setName("Advanced");
+            dataPlans2.setDownload("50");
+            dataPlans2.setUpload("10");
+            dataPlans2.setPrice("49.00");
+
+            DataPlans dataPlans3 = new DataPlans();
+            dataPlans3.setId("03");
+            dataPlans3.setName("Complete");
+            dataPlans3.setDownload("100");
+            dataPlans3.setUpload("30");
+            dataPlans3.setPrice("75.50");
+
+            DataPlans dataPlans4 = new DataPlans();
+            dataPlans4.setId("04");
+            dataPlans4.setName("UltraMax");
+            dataPlans4.setDownload("300");
+            dataPlans4.setUpload("45");
+            dataPlans4.setPrice("99.99");
+
+            DataPlans[] dataPlansList = {dataPlans1, dataPlans2, dataPlans3, dataPlans4};
+
+            return dataPlansList;
+        }else {
+            return null;
+        }
+    }
+
+    private boolean isValidPostcode(String postcode){
+        return postcode != null && postcode.matches("\\d{5}") && postcode.compareTo("00000") >= 0 && postcode.compareTo("99999") <= 0;
+    }
+}
