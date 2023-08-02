0c2e127d7ab1fe7ff52a948fe7dc36347a9f350e
paula hage <hage.paula@hotmail.com>
2023-08-02 17:58:28 +0200

    #6 Implemented the RealApiService to call the real API requests when in production environment

--- shop/api/src/main/java/com/skynet/shopapi/RealApiService.java
+++ shop/api/src/main/java/com/skynet/shopapi/RealApiService.java
@@ -1,20 +1,36 @@
-// package com.skynet.shopapi;
+package com.skynet.shopapi;
 
-// import org.springframework.stereotype.Service;
-// import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 
 
-// @Service
-// @Profile("prod")
-// public class RealApiService implements ApiService {
+@Service
+@Profile("prod")
+public class RealApiService implements ApiService {
 
-//   @Override
-//   public Address getAddressClass(String postcode) {
-//     //Call real API to get Address data
-//   }
+  private final RestTemplate restTemplate;
+  private final String apiAddressBaseUrl;
+  private final String apiDataPlansBaseUrl;
 
-//   @Override
-//   public DataPlans[] geDataPlansClass(String postcode) {
-//     // Call the real API to get DataPlans data
-//   }
-// }
+  public RealApiService(RestTemplate restTemplate, @Value("${api-address-url}") String apiAddressBaseUrl, @Value("${api-data-plans-url}") String apiDataPlansBaseUrl) {
+    this.restTemplate = restTemplate;
+    this.apiAddressBaseUrl = apiAddressBaseUrl;
+    this.apiDataPlansBaseUrl = apiDataPlansBaseUrl;
+  }
+
+  @Override
+  public Address getAddressClass(String postcode) {
+    String getAddressUrl = apiAddressBaseUrl + "getAddress?postcode=" + postcode;
+    Address address = restTemplate.getForObject(getAddressUrl, Address.class);
+    return address;
+  }
+
+  @Override
+  public DataPlans[] getDataPlansClass(String postcode) {
+    String getDataPlansUrl = apiDataPlansBaseUrl + "getPlans?postcode=" + postcode;
+    DataPlans[] dataPlansList = restTemplate.getForObject(getDataPlansUrl, DataPlans[].class);
+    return dataPlansList;
+  }
+}
