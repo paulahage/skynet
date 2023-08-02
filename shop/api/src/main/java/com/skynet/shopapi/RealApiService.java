6d0c45f9be68b3baa4e0ee7fc78839b164511d62
paula hage <hage.paula@hotmail.com>
2023-08-02 16:58:08 +0200

    #6 Created profiles for production and development to run API accordingly to each environment. For this implementation was created a ApiService interface to implement its  methods in the Profiles and to inject in the ApiController to handle the HTTP requests and return a response based in which Profile is active running

---
+++ shop/api/src/main/java/com/skynet/shopapi/RealApiService.java
@@ -0,0 +1,20 @@
+// package com.skynet.shopapi;
+
+// import org.springframework.stereotype.Service;
+// import org.springframework.context.annotation.Profile;
+
+
+// @Service
+// @Profile("prod")
+// public class RealApiService implements ApiService {
+
+//   @Override
+//   public Address getAddressClass(String postcode) {
+//     //Call real API to get Address data
+//   }
+
+//   @Override
+//   public DataPlans[] geDataPlansClass(String postcode) {
+//     // Call the real API to get DataPlans data
+//   }
+// }
